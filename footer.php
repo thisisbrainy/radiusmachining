1c7a431cbf717c2b9f0cd480917a8c2470f4e606
Brainy <brainy@brainy.blog>
2017-01-26 11:19:06 +0200

    Fix #17

--- footer.php
+++ footer.php
@@ -24,6 +24,16 @@
 <?php if(!is_home() && !is_front_page()): ?></div><?php endif; ?>
 
 <?php endif; ?>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-10264577-1', 'auto');
+ga('send', 'pageview');
+
+</script>
 
 <script src="//code.jquery.com/jquery-latest.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/jquery.swipebox.min.js"></script>
